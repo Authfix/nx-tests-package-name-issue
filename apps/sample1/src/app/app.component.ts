cc6846d469f15ff415d7dce19242e3f1e5314923
Thomas Bailly <3940727+Authfix@users.noreply.github.com>
2022-12-26 13:34:47 +0000

    chore: create basic configuration

---
+++ apps/sample1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'samples-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'sample1';
+}
